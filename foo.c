b29e277bc87d248ad168e66683526b51eebab60c
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-25 10:38:49 -0800

    Add comments for clarity.

--- foo.c
+++ foo.c
@@ -8,10 +8,12 @@ int main(void)
     // pointer comparison doesn't work b/c the strings are at different places in memory
     // printf("%d\n", s == "exit");
 
-    // compare a byte at a time
+    // let's compare these char arrays 1 byte at a time
     printf("%d\n", strcmp(s, "exit"));
-    // strcmp returns the difference between these strings (0 difference is a match)
+    // strcmp returns the difference between these strings
+    // (0 difference is a match)
 
+    // this is the way to check whether 2 strings are equal
     if (strcmp(s, "exit") == 0)
         printf("strings are the same!\n");
 }
